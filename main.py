c0110cef9142332206584b207ce137634968ed77
Ajithkumarigs <ajithkumara.igs@gmail.com>
2022-03-29 00:48:51 +0530

    pythondep

---
+++ main.py
@@ -0,0 +1,8 @@
+
+name = input("enter your name :")
+age = int(input("enter your age : "))
+
+if age >= 18:
+   print(name, "age is ", age , "Eligible for vote.")
+else:
+   print(name, "age is ", age , "Not Eligible for vote.")
